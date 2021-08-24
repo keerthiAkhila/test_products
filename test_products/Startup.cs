9b9679aa07d98192016909e9f02e48b91b718979
keerthi2544@gmail.com <keerthi2544@gmail.com>
2021-08-24 10:08:38 +0530

    Testing

--- test_products/Startup.cs
+++ test_products/Startup.cs
@@ -13,13 +13,16 @@ using System.Threading.Tasks;
 
 namespace test_products
 {
+    /// <summary>
+    /// this is for testing
+    /// </summary>
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
+         
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
